73d9319b82ff4b5edc745ef625dd63bcc8e52cae
Dan Boehmke <Daniel@webappclouds.com>
2019-11-27 14:20:01 -0500

    Handlebar helpers

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ const stories = require('./routes/stories');
 // Load Keys
 const keys = require('./config/keys');
 
+// Handlebars Helpers
+const {
+  truncate,
+  stripTags
+} = require('./helpers/hbs');
+
 // Map Global Promises
 mongoose.Promise = global.Promise;
 
@@ -38,6 +44,10 @@ app.use(bodyParser.json());
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
+  helpers: {
+    truncate: truncate,
+    stripTags: stripTags
+  },
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
