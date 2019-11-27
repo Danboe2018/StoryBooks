9d1b7debd7ca535acecc2c2da1b4fab889acf028
Dan Boehmke <Daniel@webappclouds.com>
2019-11-27 15:56:11 -0500

    Remove keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://Root:1234@vidjot-prod-owrdr.mongodb.net/storybooks?retryWrites=true&w=majority',
-  googleClientID: '1095362979012-0ko5i1583br6a0uopb0numk8phdorml8.apps.googleusercontent.com',
-  googleClientSecret: 'L-KcRw9QiEo3Tp1Cng4w_knC'
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
