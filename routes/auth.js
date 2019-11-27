7801e6d3a33fe0470bb5c8e507d1dbc4a20795b6
Dan Boehmke <Daniel@webappclouds.com>
2019-11-27 14:44:28 -0500

    Dashboard

--- routes/auth.js
+++ routes/auth.js
@@ -2,25 +2,23 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/google', passport.authenticate('google',
-  { scope: ['profile', 'email'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/dashboard');
   });
 
-router.get('/verify', (res, req) => {
+router.get('/verify', (req, res) => {
   if (req.user) {
-
+    console.log(req.user);
   } else {
-
+    console.log('Not Auth');
   }
 });
 
-// Logout User
 router.get('/logout', (req, res) => {
-  req.logOut();
+  req.logout();
   res.redirect('/');
 });
 
